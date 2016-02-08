fe55e5acf29bfb051de33fecdf8eddd77d4b94c7
rizdaputra <rizdaputra@gmail.com>
2016-02-08 13:49:56 +0700

    UI kasar added

---
+++ Inventralalab/Cek Peminjaman.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Inventralalab
+{
+    /// <summary>
+    /// Interaction logic for Cek_Peminjaman.xaml
+    /// </summary>
+    public partial class Cek_Peminjaman : Window
+    {
+        public Cek_Peminjaman()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- Inventralalab/Penambahan Alat.xaml.cs
+++ Inventralalab/Penambahan Alat.xaml.cs
@@ -10,15 +10,17 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Inventralalab {
+namespace Inventralalab
+{
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for Penambahan_Alat.xaml
     /// </summary>
-    public partial class MainWindow : Window {
-        public MainWindow() {
+    public partial class Penambahan_Alat : Window
+    {
+        public Penambahan_Alat()
+        {
             InitializeComponent();
         }
     }
